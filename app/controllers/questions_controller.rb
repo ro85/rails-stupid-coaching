ab30ee2d1804e5ed5a540ddef542d8fca1b4a62a
Rodrigo Villanueva <r_villanueva18@hotmail.com>
2020-07-20 14:11:55 -0300

    rails-stupid-coaching. Solve ccs pendiente

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  
+  def ask
+    
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question.downcase == "I am going to work right now!".downcase
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
